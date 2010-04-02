1f2612bdca67e3ec1cd6ec7592824bfa398a8ebf
Daniel <daniel.sidobre@laas.fr>
2010-04-02 08:43:10 +0200

    add kukaLBR_mgi_q_e to choose the MGI

--- src/gbmKukaLbr.c
+++ src/gbmKukaLbr.c
@@ -56,10 +56,12 @@ T67 = ( C7   -S7   0   0 )
 ( C1 -S1   0   0 )( d25  *  d27  d29 )
 ( S1  C1   0   0 )( d26  *  d28  d30 )
 (  0   0   1   0 )( d20  *  d22  d24 )
-
-
-
 */
+
+#include "math.h"
+#include <stdio.h>
+#include "gbStruct.h"
+
 typedef struct Gb_q7 {
   double q1;
   double q2;
@@ -69,38 +71,98 @@ typedef struct Gb_q7 {
   double q6;
   double q7;
 } Gb_q7;
-typedef struct Gb_v3 {
-  double x;
-  double y;
-  double z;
-} Gb_v3;
-typedef struct Gb_th {
-  Gb_v3 vx;
-  Gb_v3 vy;
-  Gb_v3 vz;
-  Gb_v3 vp;
-} Gb_th;
 
-#include "math.h"
+//   --- Debut a virer
 
-#include <stdio.h>
-
-void kukaLBR_mgd(Gb_q7* Q, Gb_q7* Q0, double r3, double r5, Gb_th* th07)
+//typedef struct Gb_v3 {
+//  double x;
+//  double y;
+//  double z;
+//} Gb_v3;
+//typedef struct Gb_th {
+//  Gb_v3 vx;
+//  Gb_v3 vy;
+//  Gb_v3 vz;
+//  Gb_v3 vp;
+//} Gb_th;
+/*
+ * Gb_v3_cross_product : compute the cros product w of u and v
+ * accepte output=u ou v
+ */
+void Gb_v3_cross_product(const Gb_v3* u, const Gb_v3* v, Gb_v3* output)
 {
-  double C1 = cos (Q->q1 - Q0->q1);
-  double C2 = cos (Q->q2 - Q0->q2);
-  double C3 = cos (Q->q3 - Q0->q3);
-  double C4 = cos (Q->q4 - Q0->q4);
-  double C5 = cos (Q->q5 - Q0->q5);
-  double C6 = cos (Q->q6 - Q0->q6);
-  double C7 = cos (Q->q7 - Q0->q7);
-  double S1 = sin (Q->q1 - Q0->q1);
-  double S2 = sin (Q->q2 - Q0->q2);
-  double S3 = sin (Q->q3 - Q0->q3);
-  double S4 = sin (Q->q4 - Q0->q4);
-  double S5 = sin (Q->q5 - Q0->q5);
-  double S6 = sin (Q->q6 - Q0->q6);
-  double S7 = sin (Q->q7 - Q0->q7);
+  double x, y, z; /* Pour output = u ou v */
+  x = u->y * v->z - u->z * v->y;
+  y = u->z * v->x - u->x * v->z;
+  z = u->x * v->y - u->y * v->x;
+  output->x = x;
+  output->y = y;
+  output->z = z;
+}
+char* Gb_statusMGI_s(Gb_statusMGI u)
+{
+  switch (u) {
+  case MGI_OK : return (char*)"MGI_OK";
+  case MGI_ERROR : return (char*)"MGI_ERROR";
+  case MGI_APPROXIMATE : return (char*)"MGI_APPROXIMATE";
+  case   MGI_SINGULAR : return (char*)"MGI_SINGULAR";
+  default : return (char*)"ERROR_SWITCH";
+  }
+}
+// fin à virer
+
+// to eventually put in gb.c
+void Gb_th_print(Gb_th *th, char* s)
+{
+  printf("%s= %g %g %g %g\n", s, th->vx.x, th->vy.x, th->vz.x, th->vp.x);
+  printf("%s= %g %g %g %g\n", s, th->vz.y, th->vx.y, th->vy.y, th->vp.y);
+  printf("%s= %g %g %g %g\n", s, th->vx.z, th->vy.z, th->vz.z, th->vp.z);
+}
+
+// end to eventually put in gb.c
+
+/*
+ * compute the configuration e1 e2 e3
+ * Inputs : 
+ *   r3, r5 : constants of the arm
+ *   eq : articulation coordinates
+ * output :
+ *   e1 : 1 ou -1
+ *   e2 : 1 ou -1
+ *   e3 : 1 ou -1
+ */
+void kukaLBR_gete1e2e3(double r3, double r5, Gb_q7* eq,
+		       int* e1, int* e2, int* e3)
+{
+  double s2 = sin(eq->q2);
+  double c2 = cos(eq->q2);
+  double c3 = cos(eq->q3);
+  double s4 = sin(eq->q4);
+  double c4 = cos(eq->q4);
+  double s6 = sin(eq->q6);
+  double d23 = c2 *c3 * s4 * r5 - s2 * (c4 * r5 + r3);
+  *e1 = (d23 >= 0) ? 1. : -1.;
+  *e2 = (s4  >= 0) ? 1. : -1.;
+  *e3 = (s6  >= 0) ? 1. : -1.;
+}
+
+void kukaLBR_mgd(Gb_q7* Q, double r3, double r5, Gb_th* th07)
+{
+  double m;
+  double C1 = cos (Q->q1);
+  double C2 = cos (Q->q2);
+  double C3 = cos (Q->q3);
+  double C4 = cos (Q->q4);
+  double C5 = cos (Q->q5);
+  double C6 = cos (Q->q6);
+  double C7 = cos (Q->q7);
+  double S1 = sin (Q->q1);
+  double S2 = sin (Q->q2);
+  double S3 = sin (Q->q3);
+  double S4 = sin (Q->q4);
+  double S5 = sin (Q->q5);
+  double S6 = sin (Q->q6);
+  double S7 = sin (Q->q7);
 
   double d1  = C6 * C7;
   double d2  = S6 * C7;
@@ -142,15 +204,22 @@ void kukaLBR_mgd(Gb_q7* Q, Gb_q7* Q0, double r3, double r5, Gb_th* th07)
   th07->vx.x = d25;
   th07->vx.y = d26;
   th07->vx.z = d20;
-  th07->vy.x = d28 * d20 - d22 * d26;
-  th07->vy.y = d22 * d25 - d27 * d20;
-  th07->vy.z = d27 * d26 - d28 * d25;
   th07->vz.x = d27;
   th07->vz.y = d28;
   th07->vz.z = d22;
   th07->vp.x = d29;
   th07->vp.y = d30;
   th07->vp.z = d24;
+  m = th07->vx.x * th07->vx.x + th07->vx.y * th07->vx.y + th07->vx.z * th07->vx.z;
+  th07->vx.x /= m;
+  th07->vx.y /= m;
+  th07->vx.z /= m;
+  Gb_v3_cross_product (&(th07->vz), &(th07->vx), &(th07->vy));
+  m = th07->vy.x * th07->vy.x + th07->vy.y * th07->vy.y + th07->vy.z * th07->vy.z;
+  th07->vy.x /= m;
+  th07->vy.y /= m;
+  th07->vy.z /= m;
+  Gb_v3_cross_product(&(th07->vx), &(th07->vy), &(th07->vz));
 }
 
 
@@ -308,26 +377,126 @@ int kukaLBR_mgi_q3(Gb_th* th07, Gb_q7* Qp, double r3, double r5, double epsilon,
   S7 = -S5*d3 + C5*d4;
   C7 = C6*d1 + S6*d2;
   q->q7 = atan2(S7, C7);
+}
 
+Gb_statusMGI kukaLBR_mgi_q_e(Gb_th* th07, Gb_q7* Qp, double r3, double r5,
+			     double epsilon, int e1, int e2, int e3,
+			     Gb_q7* q) 
+{
+  Gb_statusMGI ret = MGI_OK;
+  double C1, C2, C3, C4, C5, C6, C7;
+  double S1, S2, S3, S4, S5, S6, S7;
+  double d25 = th07->vx.x;
+  double d26 = th07->vx.y;
+  double d20 = th07->vx.z;
+  double d27 = th07->vz.x;
+  double d28 = th07->vz.y;
+  double d22 = th07->vz.z;
+  double d29 = th07->vp.x;
+  double d30 = th07->vp.y;
+  double d24 = th07->vp.z;
+  double d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15;
+  double d16, d17, d18, d19, d21, d23;
+  q->q3 = Qp->q3;
+  C3 = cos(q->q3);
+  S3 = sin(q->q3);
 
+  C4 = ( d24*d24 + d29*d29 + d30*d30 - r5*r5 - r3*r3 ) / 2. / r5 / r3;
+  if ( C4 < -1.-epsilon ) { 
+    return MGI_ERROR;
+  } else if ( C4 < -1 )  { 
+    C4 = -1;
+    S4 = 0;
+    q->q4 = M_PI;
+  } else if ( C4 < 1 )  {
+    S4 = sqrt(1 - C4*C4);
+    S4 *= e2;
+    q->q4 = atan2(S4, C4);
+  } else if ( C4 < 1 + epsilon ) {
+    C4 = 1;
+    S4 = 0;
+    q->q4 = 0;
+  } else { 
+    return MGI_ERROR;
+    // here it is possible to reteurn an approximated q...
+  }
+  d11 = S4 * r5;
+  d12 = C4 * r5;
+  d17 = C3 * d11;
+  d18 = -S3 * d11;
+  if ( (d29*d29 + d30*d30 -d18*d18) < -epsilon) {
+    return MGI_ERROR;
+  } else if ( (d29*d29 + d30*d30 -d18*d18) < 0) {
+    d23 = 0;
+  } else {
+    d23 = sqrt(d29*d29 + d30*d30 -d18*d18);
+    d23 *= e1;
+  }
+  if ((d23*d23 + d18*d18) == 0) {
+    ret = MGI_SINGULAR;
+    q->q1 = Qp->q1;
+    C1 = cos(Qp->q1);
+    S1 = sin(Qp->q1);
+  } else {
+    C1 = (d23*d29 - d18*d30) / (d23*d23 + d18*d18);
+    S1 = (d18*d29 + d23*d30) / (d23*d23 + d18*d18);
+    q->q1 = atan2(S1, C1);
+  }
+  d21 = C1 * d27 + S1 * d28;
+  d16 = S1 * d27 - C1 * d28;
+  d19 = C1 * d25 + S1 * d26;
+  d14 = S1 * d25 - C1 * d26;
+  if (( d17*d17+(d12+r3)*(d12+r3) ) == 0) {
+    ret = MGI_SINGULAR;
+    q->q2 = Qp->q2;
+    C2 = cos(Qp->q2);
+    S2 = sin(Qp->q2);
+  } else {
+    C2 = (d17*d23 + (d12+r3)*d24) / (d17*d17+(d12+r3)*(d12+r3));
+    S2 = (-(d12+r3)*d23 + d17*d24) / (d17*d17+(d12+r3)*(d12+r3));
+    q->q2 = atan2(S2, C2);
+  }
+  d15 = C2*d21+S2*d22;
+  d10 =-S2*d21+C2*d22;
+  d13 = C2*d19 + S2*d20;
+  d8 = -S2*d19 + C2*d20;
+  d9 = C3*d15 - S3*d16;
+  d6 = -S3*d15 - C3*d16;
+  d7 = C3*d13 - S3*d14;
+  d4 = -S3*d13 - C3*d14;
+  d5 = C4*d9 - S4*d10;
+  C6 = S4*d9 + C4*d10;
+  d3 = C4*d7 - S4*d8;
+  d2 = S4*d7 + C4*d8;
+  S6= sqrt(d5*d5 + d6*d6);
+  S6 *= e3;
+  q->q6 = atan2(S6, C6);
+  if (S6 == 0) {
+    ret = MGI_SINGULAR;
+    q->q6 = Qp->q6;
+    C6 = cos(Qp->q6);
+    S6 = sin(Qp->q6);
+  } else {
+    C5 = -d5/S6;
+    S5 = -d6/S6;
+    q->q5 = atan2(S5, C5);
+  }
+  d1 = C5*d3 + S5*d4;
+  S7 = -S5*d3 + C5*d4;
+  C7 = C6*d1 + S6*d2;
+  q->q7 = atan2(S7, C7);
+  return ret;
 }
 
 
 int main(int argc, char** argv) 
 {
-  Gb_q7 q, q0, qs;
+  Gb_q7 q, qs;
   double r3 = 0.4;
   double r5 = 0.39;
   Gb_th th07;
+  Gb_th thp;
   double epsilon = 1e-7;
-
-  q0.q1 = 0.0;
-  q0.q2 = 0.0;
-  q0.q3 = 0.0;
-  q0.q4 = 0.0;
-  q0.q5 = 0.0;
-  q0.q6 = 0.0;
-  q0.q7 = 0.0;
 
 //  q.q1 = 0;
 //  q.q2 = 0;
@@ -353,10 +522,70 @@ int main(int argc, char** argv)
 //
   printf("q= %g %g %g  %g %g %g  %g\n", q.q1, q.q2, q.q3, q.q4, q.q5, q.q6, q.q7);
 
-  kukaLBR_mgd(&q, &q0, r3, r5, &th07);
+  kukaLBR_mgd(&q, r3, r5, &th07);
+  Gb_th_print(&th07, "th07");
   kukaLBR_mgi_q3(&th07, &q, r3, r5, epsilon, &qs);
 
   printf("qs= %g %g %g  %g %g %g  %g\n", 
 	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+
+  int e1, e2, e3;
+  Gb_statusMGI status;
+  kukaLBR_gete1e2e3(r3, r5, &q, &e1, &e2, &e3);
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  printf("\n");
+  e1 = 1; e2 = 1; e3 = 1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 = 1; e2 = 1; e3 =-1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 = 1; e2 =-1; e3 = 1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 = 1; e2 =-1; e3 =-1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 =-1; e2 = 1; e3 = 1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 =-1; e2 = 1; e3 =-1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 =-1; e2 =-1; e3 = 1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
+  e1 =-1; e2 =-1; e3 =-1;
+  status = kukaLBR_mgi_q_e(&th07, &q, r3, r5, epsilon, e1, e2, e3, &qs);
+  printf("%s: %d%d%d qs= %g %g %g  %g %g %g  %g\n", 
+	 Gb_statusMGI_s(status), e1, e2, e3,
+	 qs.q1, qs.q2, qs.q3, qs.q4, qs.q5, qs.q6, qs.q7);
+  kukaLBR_mgd(&qs, r3, r5, &thp);   Gb_th_print(&thp, "th07");
   return 0;
 }
+
